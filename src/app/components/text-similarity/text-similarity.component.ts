8937057e1cf897356826729b77a183264e5739a4
Lazar Dejanovic <ldejanovic8120rn@raf.rs>
2022-10-24 01:22:26 +0200

    finished text similarity component and service

--- src/app/components/text-similarity/text-similarity.component.ts
+++ src/app/components/text-similarity/text-similarity.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TextSimilarityService} from "../../services/text-similarity.service";
 
 @Component({
   selector: 'app-text-similarity',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TextSimilarityComponent implements OnInit {
 
-  constructor() { }
+  text1: string = '';
+  text2: string = '';
+  similarity: number = -1;
+
+  constructor(private textSimilarityService: TextSimilarityService) { }
 
   ngOnInit(): void {
+  }
+
+  compareTexts() {
+    this.textSimilarityService.getTextSimilarity(this.text1, this.text2).subscribe((textSimilarity) => {
+      this.similarity = textSimilarity.similarity;
+    })
+  }
+
+  hasText1(): boolean {
+    return this.text1.length == 0;
+  }
+
+  hasText2(): boolean {
+    return this.text2.length == 0;
   }
 
 }
--- src/app/services/text-similarity.service.ts
+++ src/app/services/text-similarity.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {TextSimilarity} from "../models/text-similarity";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TextSimilarityService {
 
-  constructor() { }
+  private readonly textSimilarityApi = environment.textSimilarityApi;
+
+  constructor(private httpClient: HttpClient) { }
+
+  getTextSimilarity(text1: string, text2: string): Observable<TextSimilarity> {
+    let url: string = this.textSimilarityApi + "/?text1=" + text1 + "&text2=" + text2 + "&token=" + localStorage.getItem("token");
+
+    return this.httpClient.get<TextSimilarity>(url);
+  }
 }
