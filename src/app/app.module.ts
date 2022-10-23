fe88a1124e6751c19d63471928603dc201572990
Lazar Dejanovic <ldejanovic8120rn@raf.rs>
2022-10-23 20:02:11 +0200

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { ConfigurationComponent } from './components/configuration/configuration.component';
+import { EntityExtractionComponent } from './components/entity-extraction/entity-extraction.component';
+import { TextSimilarityComponent } from './components/text-similarity/text-similarity.component';
+import { LanguageDetectionComponent } from './components/language-detection/language-detection.component';
+import { SentimentAnalysisComponent } from './components/sentiment-analysis/sentiment-analysis.component';
+import { HistoryComponent } from './components/history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfigurationComponent,
+    EntityExtractionComponent,
+    TextSimilarityComponent,
+    LanguageDetectionComponent,
+    SentimentAnalysisComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
