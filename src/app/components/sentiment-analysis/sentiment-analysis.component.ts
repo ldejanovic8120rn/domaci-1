abc21f202ff17e9f926cf27e537fdf227ddf8027
Lazar Dejanovic <ldejanovic8120rn@raf.rs>
2022-10-25 05:33:26 +0200

    finished sentiment analysis component and service

--- src/app/components/sentiment-analysis/sentiment-analysis.component.ts
+++ src/app/components/sentiment-analysis/sentiment-analysis.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {SentimentAnalysisService} from "../../services/sentiment-analysis.service";
+import {Color} from "../../models/color";
 
 @Component({
   selector: 'app-sentiment-analysis',
@@ -7,9 +9,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SentimentAnalysisComponent implements OnInit {
 
-  constructor() { }
+  text: string = "";
+  lang: string = "en";
+
+  score: number | undefined;
+  type: string | undefined;
+
+  colorGreen: Color = {
+    r: 0,
+    g: 255,
+    b: 0
+  };
+  colorRed: Color = {
+    r: 255,
+    g: 0,
+    b: 0
+  };
+  colorType: Color = {
+    r: 0,
+    g: 0,
+    b: 0
+  }
+
+  constructor(private sentimentAnalysisService: SentimentAnalysisService) { }
 
   ngOnInit(): void {
+  }
+
+  getAnalysis() {
+    this.sentimentAnalysisService.getSentimentAnalysis(this.text, this.lang).subscribe((analysis) => {
+      this.score = analysis.sentiment.score;
+      this.type = analysis.sentiment.type;
+
+      // (score - lowerB) / (upperB - lowerB)
+      let lowerB = -1;
+      let upperB = 1;
+      let normalizedScore = (this.score - lowerB) / (upperB - lowerB);
+
+      this.colorType = {
+        r: this.colorRed.r + (this.colorGreen.r - this.colorRed.r) * normalizedScore,
+        g: this.colorRed.g + (this.colorGreen.g - this.colorRed.g) * normalizedScore,
+        b: this.colorRed.b + (this.colorGreen.b - this.colorRed.b) * normalizedScore
+      }
+
+    })
+  }
+
+  hasText(): boolean {
+    return this.text.length == 0;
+  }
+
+  getColorCode(): string {
+    return "rgb(" + this.colorType.r + ", " + this.colorType.g + ", " + this.colorType.b + ")";
   }
 
 }
--- src/app/services/sentiment-analysis.service.ts
+++ src/app/services/sentiment-analysis.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {SentimentAnalysis} from "../models/sentiment-analysis";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SentimentAnalysisService {
 
-  constructor() { }
+  private readonly sentimentAnalysisApi = environment.sentimentAnalysisApi;
+
+  constructor(private httpClient: HttpClient) { }
+
+  getSentimentAnalysis(text: string, lang: string): Observable<SentimentAnalysis> {
+    let url: string = this.sentimentAnalysisApi + "/?lang=" + lang + "&text=" + text + "&token=" + localStorage.getItem("token");
+
+    return this.httpClient.get<SentimentAnalysis>(url);
+  }
+
 }
